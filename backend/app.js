86fed44bdf64ce17eda5b74e625e695206b5f2ef
Manmeet singh <1.singhmanmeet@gmail.com>
2018-06-24 14:39:46 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ var express=require('express');
 var app=express();
 var frequencyController=require('./frequencyController');
 
-// Allow COR to this application.
+// Allow COR(Cross Origin Request) to this application.
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
