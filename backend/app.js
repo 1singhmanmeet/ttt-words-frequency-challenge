cb658721e52f84225e2c8678790efdc4b398fd22
Manmeet singh <1.singhmanmeet@gmail.com>
2018-06-24 14:39:12 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,14 @@ var express=require('express');
 var app=express();
 var frequencyController=require('./frequencyController');
 
+// Allow COR to this application.
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
   
+// Redirecting the "/frequency" request to frequencyController.
 app.use("/frequency",frequencyController);
 
 module.exports=app;
